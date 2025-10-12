74ed960ace21b203ecc7a3e059ab1b48bc94c1c5
Joel DSouza <joeldsouzax@gmail.com>
2025-10-12 17:04:46 +0200

    chore: added tracing and env

--- mastodon/src/main.rs
+++ mastodon/src/main.rs
@@ -1,4 +1,16 @@
+use tracing_subscriber::{
+    EnvFilter, Layer,
+    fmt::{self, format::FmtSpan},
+    prelude::*,
+};
+
 #[tokio::main]
 async fn main() {
+    let filter = EnvFilter::from_default_env();
+    let console = fmt::layer()
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_filter(filter);
+    tracing_subscriber::registry().with(console).init();
     println!("Hello, world!");
 }
