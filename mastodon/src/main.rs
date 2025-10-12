8dbc2d7b5e3f787ddae3f756e371a25258a1909e
Joel DSouza <joeldsouzax@gmail.com>
2025-10-12 18:13:40 +0200

    chore: added kafka compose

--- mastodon/src/main.rs
+++ mastodon/src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+use std::env;
 use tracing_subscriber::{
     EnvFilter, Layer,
     fmt::{self, format::FmtSpan},
@@ -5,12 +7,22 @@ use tracing_subscriber::{
 };
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let filter = EnvFilter::from_default_env();
     let console = fmt::layer()
         .with_level(true)
         .with_span_events(FmtSpan::CLOSE)
         .with_filter(filter);
     tracing_subscriber::registry().with(console).init();
-    println!("Hello, world!");
+
+    let mastodon_url = env::var("MASTODON_URL")?;
+    let mastodon_access_token = env::var("MASTODON_ACCESS_TOKEN")?;
+
+    Ok(())
 }
+
+// TODO: a client server which creates a websocket request and fetchs statuses.
+// TODO: listens to tokio ctrl+c to stop the connection and gracefully stops the server
+
+// should return a stream of posts
+pub trait SocialFeeder {}
