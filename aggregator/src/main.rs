24891235598391ee11404dd0afb32ca3ed82f25e
Joel DSouza <joeldsouzax@gmail.com>
2025-10-12 23:05:45 +0200

    chore: added swagger and axum

--- aggregator/src/main.rs
+++ aggregator/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
+use anyhow::Result;
+use std::net::Ipv4Addr;
+use tokio::net::TcpListener;
+use tracing::instrument;
+use tracing_subscriber::{
+    EnvFilter, Layer,
+    fmt::{self, format::FmtSpan},
+    prelude::*,
+};
+use utoipa::OpenApi;
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_swagger_ui::SwaggerUi;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let filter = EnvFilter::from_default_env();
+    let console = fmt::layer()
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_filter(filter);
+    tracing_subscriber::registry().with(console).init();
     println!("Hello, world!");
+
+    Ok(())
+}
+
+#[instrument(level = "debug", err)]
+async fn app(port: u16) -> Result<()> {
+    let tcp = TcpListener::bind((Ipv4Addr::new(0, 0, 0, 0), port)).await?;
+    let (router, api) = OpenApiRouter::with_openapi(api::ApiDoc::openapi())
+        .merge(api::router())
+        .split_for_parts();
+    let app = router.merge(SwaggerUi::new("/swagger").url("/api-docs/openapi.json", api));
+    Ok(())
 }
