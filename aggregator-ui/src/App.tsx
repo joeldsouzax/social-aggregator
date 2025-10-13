0751fb76cebfa25778d1c38ae1e4e42b743a2959
Joel DSouza <joeldsouzax@gmail.com>
2025-10-13 23:10:39 +0200

    chore: made posts list pretty :D

--- aggregator-ui/src/App.tsx
+++ aggregator-ui/src/App.tsx
@@ -1,7 +1,8 @@
 import { usePostQuery } from '@/services/aggregator';
+import { Container, CssBaseline, Paper, Typography, List,  ListItemButton,  ListItemText,  } from '@mui/material';
 
 export const App = () => {    
-  const { data: posts, isLoading, error } = usePostQuery('posts', { skip: false,  keepUnusedDataFor: 0 });
+  const { data: posts, isLoading, error, isFetching } = usePostQuery('posts', { skip: false,  keepUnusedDataFor: 0 });
     
   if (isLoading) {
     return <div>Connecting to the post stream...</div>;
@@ -14,15 +15,22 @@ export const App = () => {
     console.log(posts);
 
   return (
-    <div>
-      <h1>Live Post Stream</h1>
-      <ul>
-        {posts?.map((post, index) => (
-          <li key={index}>{post}</li>
-        ))}
-      </ul>
-      {(!posts || posts.length === 0) && <p>Waiting for the first post...</p>}
-    </div>
+    <Container maxWidth="sm">
+          <CssBaseline />
+      <Paper square sx={{ pb: '50px' }}>
+        <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0 }}>
+          Inbox
+        </Typography>
+        <List sx={{ mb: 2 }}>
+          {posts?.map((post, index) => (
+            <ListItemButton key={index}>
+                <ListItemText primary={post} />
+            </ListItemButton>
+          ))}
+        </List>
+      </Paper>
+    {(!posts || posts.length === 0) && <p>Waiting for the first post...</p>}
+    </Container>
   );
 };
 
