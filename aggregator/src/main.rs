87b8b41add636e5a1c7edd0d799e8cc8739d148d
Joel DSouza <joeldsouzax@gmail.com>
2025-10-13 16:40:52 +0200

    chore: wired up axum application

--- aggregator/src/main.rs
+++ aggregator/src/main.rs
@@ -1,5 +1,6 @@
 use aggregator::{ApiDoc, router};
 use anyhow::Result;
+use axum::{Router, serve::Serve};
 use std::{env, net::Ipv4Addr};
 use tokio::net::TcpListener;
 use tracing::instrument;
@@ -20,17 +21,18 @@ async fn main() -> Result<()> {
         .with_span_events(FmtSpan::CLOSE)
         .with_filter(filter);
     tracing_subscriber::registry().with(console).init();
-    let port = env::var("")?;
+    let port = env::var("PORT")?;
+    let port = port.parse::<u16>()?;
+    let _ = app(port).await?;
     Ok(())
 }
 
 #[instrument(level = "debug", err)]
-async fn app(port: u16) -> Result<()> {
+async fn app(port: u16) -> Result<Serve<TcpListener, Router, Router>> {
     let listener = TcpListener::bind((Ipv4Addr::new(0, 0, 0, 0), port)).await?;
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
         .merge(router())
         .split_for_parts();
     let app = router.merge(SwaggerUi::new("/swagger").url("/api-docs/openapi.json", api));
-    axum::serve(listener, app).await?;
-    Ok(())
+    Ok(axum::serve(listener, app))
 }
