d4556201b74fc2a8436d3ee44927ea8f63707fb0
Joel DSouza <joeldsouzax@gmail.com>
2025-10-14 23:46:09 +0200

    chore: cleaned up feeder mod

--- feeders/src/feeder/mod.rs
+++ feeders/src/feeder/mod.rs
@@ -1,37 +1,12 @@
-use rdkafka::{
-    config::ClientConfig,
-    producer::{FutureProducer, FutureRecord},
-};
-use schema_registry_converter::async_impl::proto_raw::ProtoRawEncoder;
-use schema_registry_converter::async_impl::schema_registry::SrSettings;
+pub mod builder;
+pub mod queue;
+
+use crate::error::Error;
+use queue::FeederQueue;
+use rdkafka::producer::FutureProducer;
 use std::fmt::Debug;
-use tokio::sync::mpsc::{Receiver, Sender, channel};
-use tracing::{debug, instrument};
-
-#[derive(Debug, Clone)]
-pub struct FeederQueue<T>
-where
-    T: Debug,
-{
-    tx: Sender<T>,
-}
-
-impl<T> FeederQueue<T>
-where
-    T: Debug,
-{
-    pub fn create(tx: Sender<T>) -> Self {
-        Self { tx }
-    }
-
-    #[instrument(level = "info", err)]
-    pub async fn send(&self, message: T) -> Result<(), error::Error> {
-        self.tx
-            .send(message)
-            .await
-            .map_err(|err| error::Error::FeederSend(err.to_string()))
-    }
-}
+use tokio::sync::mpsc::Receiver;
+use tracing::instrument;
 
 pub struct Feeder<T>
 where
@@ -50,61 +25,10 @@ where
     }
 
     #[instrument(level = "debug", skip(self), err)]
-    pub async fn run(self, topic: &str) -> Result<(), error::Error> {
+    pub async fn run(self, topic: &str) -> Result<(), Error> {
         let Feeder { producer, mut recv } = self;
         while let Some(message) = recv.recv().await {}
         Ok(())
-    }
-}
-
-pub struct FeederBuilder<T>
-where
-    T: Debug,
-{
-    producer: Option<FutureProducer>,
-    recv: Option<Receiver<T>>,
-    sender: Option<FeederQueue<T>>,
-}
-
-impl<T> FeederBuilder<T>
-where
-    T: Debug + Clone,
-{
-    pub fn new() -> Self {
-        Self {
-            producer: None,
-            recv: None,
-            sender: None,
-        }
-    }
-
-    #[instrument(level = "debug", skip(brokers, self) err)]
-    pub fn with_brokers<S: AsRef<str>>(self, brokers: S) -> Result<Self, error::Error> {
-        debug!("creating a producer targeted at: {}", brokers.as_ref());
-        let producer: FutureProducer = ClientConfig::new()
-            .set("bootstrap.servers", brokers.as_ref())
-            .set("message.timeout.ms", "5000")
-            .create()?;
-        Ok(Self {
-            producer: Some(producer),
-            recv: None,
-            sender: None,
-        })
-    }
-
-    pub fn with_buffer(self, buffer: usize) -> Self {
-        let (tx, rx) = channel::<T>(buffer);
-        let producer = self.producer;
-        Self {
-            producer,
-            sender: Some(FeederQueue::create(tx)),
-            recv: Some(rx),
-        }
-    }
-
-    // TODO: returns a feederqueue and Feeder
-    pub fn build(self) -> Result<(), error::Error> {
-        unimplemented!()
     }
 }
 
