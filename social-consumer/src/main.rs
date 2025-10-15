33cf08bf7ff926b86fde78c882056de9fd1fb732
Joel DSouza <joeldsouzax@gmail.com>
2025-10-15 13:29:16 +0200

    chore: added tracing to consumer

--- social-consumer/src/main.rs
+++ social-consumer/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use anyhow::Result;
+use tracing::instrument;
+use tracing_subscriber::{
+    EnvFilter, Layer,
+    fmt::{self, format::FmtSpan},
+    prelude::*,
+};
+
+#[instrument]
+#[tokio::main]
+async fn main() -> Result<()> {
+    let filter = EnvFilter::from_default_env();
+    let console = fmt::layer()
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .with_filter(filter);
+    tracing_subscriber::registry().with(console).init();
     println!("Hello, world!");
+
+    Ok(())
 }
